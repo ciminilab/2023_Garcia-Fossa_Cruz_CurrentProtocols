402ed87f6fabdab2531273325b7f883a09c1b7f3
Fernanda Fossa <fefossa@gmail.com>
2022-09-25 16:52:38 -0400

    Create setup.py

---
+++ utils/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='utils',
+      version='1.0',
+      description='Plot images from high content screenings.',
+      author='Fernanda Fossa',
+      author_email='fefossa@gmail.com',
+      url='https://github.com/fefossa/2022_Garcia-Fossa_submitted',
+     )
